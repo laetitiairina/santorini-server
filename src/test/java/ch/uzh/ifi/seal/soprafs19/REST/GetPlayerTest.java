7c584e9145966dafca7320dcceb9f5b29c47b297
Andreas Holgersson <anholg.uzh@gmail.com>
2019-03-30 14:47:21 +0100

    [TESTS][REST]: Add get player test

---
+++ src/test/java/ch/uzh/ifi/seal/soprafs19/REST/GetPlayerTest.java
@@ -0,0 +1,89 @@
+package ch.uzh.ifi.seal.soprafs19.REST;
+
+import ch.uzh.ifi.seal.soprafs19.Application;
+import ch.uzh.ifi.seal.soprafs19.entity.Player;
+import ch.uzh.ifi.seal.soprafs19.entity.User;
+import ch.uzh.ifi.seal.soprafs19.repository.PlayerRepository;
+import ch.uzh.ifi.seal.soprafs19.service.PlayerService;
+import ch.uzh.ifi.seal.soprafs19.service.UserService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the REST interface.
+ *
+ * /players/{id} GET
+ */
+@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= Application.class)
+public class GetPlayerTest {
+
+    @Qualifier("playerRepository")
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private PlayerService playerService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mvc;
+
+    private User testUser;
+
+    private Player testPlayer;
+
+    @Before
+    public void setup() throws Exception {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+
+        User user = new User();
+        user.setUsername("testUsername");
+        user.setPassword("testPassword");
+
+        testUser = userService.createUser(user);
+
+        Player player = new Player();
+        player.setUserId(testUser.getId());
+        player.setMode(0);
+
+        testPlayer = playerService.createPlayer(player);
+
+    }
+
+    @Test
+    public void getPlayerCorrect() throws Exception {
+
+        Assert.assertNotNull(playerRepository.findByUserId(testUser.getId()));
+
+        mvc.perform(get("/players/"+testPlayer.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.id").value(testPlayer.getId()))
+                .andExpect(jsonPath("$.userId").value(testUser.getId()))
+                .andExpect(jsonPath("$.gameId").exists())
+                .andExpect(jsonPath("$.mode").value(testPlayer.getMode()))
+                .andExpect(jsonPath("$.card").isEmpty())
+                .andExpect(jsonPath("$.color").isEmpty());
+    }
+}
