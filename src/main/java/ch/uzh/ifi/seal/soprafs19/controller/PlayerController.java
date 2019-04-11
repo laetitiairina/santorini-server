94875eda4f894cd0ce53f2ae9203f2403f5c074f
Andreas Holgersson <anholg.uzh@gmail.com>
2019-04-11 17:31:16 +0200

    G10-33 Setup REST interface (backend) for matchmaking

--- src/main/java/ch/uzh/ifi/seal/soprafs19/controller/PlayerController.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/controller/PlayerController.java
@@ -3,18 +3,25 @@ package ch.uzh.ifi.seal.soprafs19.controller;
 import ch.uzh.ifi.seal.soprafs19.entity.Player;
 import ch.uzh.ifi.seal.soprafs19.repository.PlayerRepository;
 import ch.uzh.ifi.seal.soprafs19.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class PlayerController {
 
-    private final PlayerService service;
+    @Autowired
+    private PlayerService service;
 
+    /*
     PlayerController(PlayerService service) {
         this.service = service;
     }
+    */
 
     /*
     @GetMapping("/players")
@@ -29,11 +36,28 @@ public class PlayerController {
     }
 
     @GetMapping("/players/{id}")
-    ResponseEntity getPlayer(@PathVariable Long id, @RequestParam("fields") String fields) {
+    ResponseEntity getPlayer(@PathVariable Long id, @RequestParam(required = false) List<String> fields) {
 
-        // TODO: get player by id
+        // TODO: Authentication: Check if token sent in header matches token stored in player with requested id
 
-        return ResponseEntity.status(HttpStatus.OK).build();
+        // Get player by id
+        Optional<Player> player = service.getPlayerById(id);
+
+        // Check if player exists
+        if (player.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Player was not found!");
+        }
+
+        // If specific field was requested, only send that field of player entity
+        if (fields != null) {
+            for (String param : fields) {
+                // TODO: Return only requested parameters of player entity
+                return ResponseEntity.ok(player.get());
+            }
+        }
+
+        // Send response 200
+        return ResponseEntity.ok(player.get());
     }
 
     @PutMapping("/players/{id}")
