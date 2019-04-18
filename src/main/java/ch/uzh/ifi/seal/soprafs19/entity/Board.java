90eeb9424127cd2df3322d74c8d623ea1155c513
Laetitia Britschgi <laetitia.irina@bluewin.ch>
2019-04-18 10:32:58 +0200

    correction loop
    Signed-off-by: Laetitia Britschgi <laetitia.irina@bluewin.ch>

--- src/main/java/ch/uzh/ifi/seal/soprafs19/entity/Board.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/entity/Board.java
@@ -47,7 +47,7 @@ public class Board implements Serializable {
 
 		for (int i = 0; i < numberOfRows; i++) {
 			for (int j = 0; j < numberOfRows; j++)
-			this.fields.add(new Field(i, i));
+			this.fields.add(new Field(i, j));
 		}
 
 	}
