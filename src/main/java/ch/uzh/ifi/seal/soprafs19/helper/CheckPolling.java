432a1af8bfc03735ac4cb2161ce2c719d9cc8e61
Andreas Holgersson <anholg.uzh@gmail.com>
2019-05-08 19:15:28 +0200

    G10-85 G10-86 Decrease allowed time of last poll when player in queue

--- src/main/java/ch/uzh/ifi/seal/soprafs19/helper/CheckPolling.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/helper/CheckPolling.java
@@ -1,7 +1,5 @@
 package ch.uzh.ifi.seal.soprafs19.helper;
 
-import ch.uzh.ifi.seal.soprafs19.repository.GameRepository;
-import ch.uzh.ifi.seal.soprafs19.repository.PlayerRepository;
 import ch.uzh.ifi.seal.soprafs19.service.GameService;
 import ch.uzh.ifi.seal.soprafs19.service.PlayerService;
 import ch.uzh.ifi.seal.soprafs19.constant.GameStatus;
@@ -13,8 +11,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Component
 @Transactional
 public class CheckPolling implements Runnable {
@@ -25,22 +21,27 @@ public class CheckPolling implements Runnable {
     @Autowired
     private GameService gameService;
 
+    // 1 seconds
+    private int QUEUE_POLL_TIME_MAX = 1000;
+
     // 10 seconds
-    private int POLL_TIME_MAX = 10000;
+    private int GAME_POLL_TIME_MAX = 10000;
 
     @Scheduled(fixedRate = 1000)
     @Async("taskExecutor")
     public void run() {
         System.out.println("Check polling");
         for (Player player:playerService.getAllActivePlayers()) {
-            if (player.lastPoll() > POLL_TIME_MAX) {
-                // still in queue
-                if (player.getGame() == null) {
+            // still in queue
+            if (player.getGame() == null) {
+                if (player.lastPoll() > QUEUE_POLL_TIME_MAX) {
                     // Remove player from queue if player hasn't polled in x seconds
                     System.out.println("Player removed from queue");
                     playerService.abortSearch(player);
-                // playing game
-                } else if (player.getGame().getStatus() != GameStatus.END) {
+                }
+            // playing game
+            } else if (player.getGame().getStatus() != GameStatus.END) {
+                if (player.lastPoll() > GAME_POLL_TIME_MAX) {
                     // Abort game if player hasn't polled in x seconds
                     System.out.println("Game aborted");
                     gameService.abortGame(player.getGame());
