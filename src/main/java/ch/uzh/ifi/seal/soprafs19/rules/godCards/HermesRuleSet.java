c66c7897a574e076a2e860a13b3c509fb90eb113
Can Inan <can.inan@bluewin.ch>
2019-05-17 00:16:55 +0200

    G10-89 # changed method and implemented rekursiv

--- src/main/java/ch/uzh/ifi/seal/soprafs19/rules/godCards/HermesRuleSet.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/rules/godCards/HermesRuleSet.java
@@ -10,17 +10,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class HermesRuleSet extends SimpleRuleSet {
 
-    public Boolean isValidMoveHermes(boolean isSecondMove, Field fieldBefore, Field fieldAfter) {
-        // origin field had a worker or it's the second move of a worker
-        if ((fieldBefore.getWorker() != null) || isSecondMove
-                // destination field is unoccupied
-                && (fieldAfter.getWorker() == null)
-                // destination field has no dome
-                && (!fieldAfter.getHasDome())) {
-            //check if blocks in after field are the same
-            return (fieldAfter.getBlocks() == fieldBefore.getBlocks());
+    //Rekursiv gemäss laeti
+    public boolean hermesPath(Game before, int blockLevel, long destinationId, Field field) {
+        for (Field neighbour : neighbouringFields(before, field.getPosX(), field.getPosY())) {
+            if (neighbour.getBlocks() == blockLevel) {
+                if (neighbour.getId() == destinationId) {
+                    return true;
+                } else {
+                    return hermesPath(before, blockLevel, destinationId, neighbour);
+                }
+            }
         }
-
         return false;
     }
 
@@ -28,23 +28,16 @@ public class HermesRuleSet extends SimpleRuleSet {
     public Boolean checkMovePhase(Game before, Game after) {
 
         //when player wants to move up or down with worker-> normal move
-        if (setFieldsAfterMovePhase(after) && setFieldsBeforeMovePhase(before)) {
+        if (setFieldsAfterMovePhase(before) && setFieldsBeforeMovePhase(after)) {
             if (fieldAfter.getBlocks() != fieldBefore.getBlocks()) {
                 return isFieldFree(before, fieldBeforeBackEnd, fieldAfterBackEnd, false);
 
             }
 
             //special hermes Move-> on the same level, worker can move as much as he wants and can also stay on same place
-            else
-                for(Field field : neighbouringFields(before,fieldBefore.getPosX(),fieldBefore.getPosY())){
-                        if(isValidMoveHermes(false, fieldBefore, field)){
-                            fieldBefore = field;
-                            if(fieldBefore.getId() == fieldAfter.getId())
-                                return true;
-                        }
-                }
-
+            else return hermesPath(before, fieldBefore.getBlocks(), fieldAfter.getId(), fieldBefore);
         }
+
         return false;
     }
 }
