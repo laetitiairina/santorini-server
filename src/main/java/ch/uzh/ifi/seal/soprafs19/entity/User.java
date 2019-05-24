5c8800c49d514c33c28fcfe6cded12db5174f90d
Elliott Wallace <elliott.d.wallace@gmail.com>
2019-05-24 14:02:40 +0200

    reset server

--- src/main/java/ch/uzh/ifi/seal/soprafs19/entity/User.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/entity/User.java
@@ -14,7 +14,6 @@ import javax.persistence.*;
 @EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable {
 	
-
 	private static final long serialVersionUID = 1L;
 
 	@Id
